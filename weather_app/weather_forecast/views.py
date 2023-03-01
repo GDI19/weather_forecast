2faa75f84a12d87daa82524223cf252aa04520d6
GDI <dimon.1982@mail.ru>
2023-03-01 13:55:54 +0700

    add css and img

--- weather_app/weather_forecast/views.py
+++ weather_app/weather_forecast/views.py
@@ -12,58 +12,63 @@ def index(request):
         form = CityForm(request.POST)
         try:
             form.is_valid()
-            context = get_data(form.cleaned_data['name'])
+            city = form.cleaned_data['name']
+            data_from_api = get_data(city)
+            city_info = fill_city_info(data_from_api, city)
+            five_days_forecast = fill_weather_forecast(data_from_api)
+            form = CityForm()
+            context = {'weather': five_days_forecast, 'city_info': city_info, 'form': form}
             return render(request, 'weather_forecast/index.html', context)
         except:
             messages.error(request, "City name is not correct. Please try to enter correct name.")
-            # return redirect(index)
 
-    #city = 'Moscow'
-    #context = get_data(city)
     form = CityForm()
     context = {'form': form}
     return render(request, 'weather_forecast/index.html', context)
 
 
 def get_data(city):
-    days_index_list = [0, 8, 16, 24, 32, 39]
-    weather_5days_list = []
     url_geo = 'http://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid=6f9b5463e6469f3078746f08677709b4'
     coordinates_from_api = requests.get(url_geo.format(city)).json()
-    # print(coordinates_from_api)
     latitude = round(coordinates_from_api[0]['lat'], 2)
     longitude = round(coordinates_from_api[0]['lon'], 2)
-    #print(type(latitude), longitude)
 
     url_5days = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=6f9b5463e6469f3078746f08677709b4&units=metric'
     data_from_api = requests.get(url_5days.format(lat=latitude, lon=longitude)).json()
-    # print(data_from_api)
 
+    return data_from_api
+
+
+def fill_city_info(data_from_api, city):
     timezone_sec = int(data_from_api['city']['timezone'])
     timezone = timezone_sec / 3600
-    sunrise_local = datetime.utcfromtimestamp((data_from_api['city']['sunrise'] + timezone_sec))
-    sunset_local = datetime.utcfromtimestamp((data_from_api['city']['sunset'] + timezone_sec))
-    #now_utc = time.time()
-    #local_time_in_mlsec = (now_utc + timezone_sec) * 1000
-    #local_time = datetime.utcfromtimestamp(local_time_in_sec)
+    sunrise_local = datetime.utcfromtimestamp((data_from_api['city']['sunrise'] + timezone_sec)).strftime("  %d %b. %Y, %H:%M")
+    sunset_local = datetime.utcfromtimestamp((data_from_api['city']['sunset'] + timezone_sec)).strftime("  %d %b. %Y, %H:%M")
 
+    url_for_country = 'https://restcountries.com/v3.1/alpha/{}'
+    country_code = data_from_api['city']['country']
+    data_for_country = requests.get(url_for_country.format(country_code)).json()
+    country_name = data_for_country[0]['name']['common']
 
     city_info = {
         'name': city,
-        'country': data_from_api['city']['country'],
+        'country_name': country_name,
+        'country_code': country_code,
         'timezone': timezone,
         'sunrise': sunrise_local,
         'sunset': sunset_local,
-        # 'population': data_from_api['city']['population'],
-        #'local_time_in_sec': local_time_in_mlsec,
-
-
     }
+    return city_info
+
+
+def fill_weather_forecast(data_from_api):
+    days_index_list = [0, 8, 16, 24, 32, 39]
+    weather_5days_list = []
 
     for d in days_index_list:
         item = data_from_api['list'][d]
         datetime_object = datetime.strptime(item['dt_txt'], '%Y-%m-%d %H:%M:%S')
-        day = datetime_object.strftime('%a, %d/%m/%Y')
+        day = datetime_object.strftime('%a, %d %b. %Y')
         # print(day)
         weather_dict = {
                 'day': day,
@@ -76,6 +81,4 @@ def get_data(city):
             }
         weather_5days_list.append(weather_dict)
 
-    form = CityForm()
-    context = {'weather': weather_5days_list, 'city_info': city_info, 'form': form}
-    return context
+    return weather_5days_list
